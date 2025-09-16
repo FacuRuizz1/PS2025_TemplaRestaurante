db259114bc2b334253b6b8f827389f4982ae0ff7
114264 - Moszoro Mateo <114264@tecnicatura.frc.utn.edu.ar>
2025-09-16 19:29:45 -0300

    refactor del modelo persona
    Se refactorizo el nombre del Modelo de Persona

--- BackEnd/TemplaRestaurant/src/main/java/Templa/Tesis/App/models/Persona.java
+++ BackEnd/TemplaRestaurant/src/main/java/Templa/Tesis/App/models/Persona.java
@@ -1,19 +1,16 @@
 package Templa.Tesis.App.models;
 
 import Templa.Tesis.App.Enums.TipoPersona;
-import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PersonaModel {
+public class Persona {
     private String nombre;
     private String apellido;
     private String email;
