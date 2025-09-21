e1808563449a1452c67c6d1d850fab7949e83d70
114264 - Moszoro Mateo <114264@tecnicatura.frc.utn.edu.ar>
2025-09-21 15:29:04 -0300

    Usuario-modal

---
+++ TemplaFront/src/app/componentes/modales/usuario-modal/usuario-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario-modal.component.html',
+  styleUrl: './usuario-modal.component.css'
+})
+export class UsuarioModalComponent {
+
+}
