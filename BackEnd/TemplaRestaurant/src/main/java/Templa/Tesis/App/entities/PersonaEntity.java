c4e6871f4693cdfeab21467b42a63e2e7418a4bc
114318-Ruiz, Facundo Nicolas <114318@tecnicatura.frc.utn.edu.ar>
2025-09-26 10:25:22 -0300

    test usuarioServiceImpl

--- BackEnd/TemplaRestaurant/src/main/java/Templa/Tesis/App/entities/PersonaEntity.java
+++ BackEnd/TemplaRestaurant/src/main/java/Templa/Tesis/App/entities/PersonaEntity.java
@@ -3,6 +3,7 @@ package Templa.Tesis.App.entities;
 import Templa.Tesis.App.Enums.TipoPersona;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class PersonaEntity {
 
     @Id
